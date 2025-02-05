1aa91eba7fcb4b867657bae8d654121514cb7597
steffkes <stefan@gelungen.es>
2025-02-06 00:14:19 +0100

    show personal personal bests

--- pages/persoenliche-bestzeiten/[name]/[year].vue
+++ pages/persoenliche-bestzeiten/[name]/[year].vue
@@ -34,13 +34,40 @@
 
   <footer class="footer mt-6">
     <div class="content has-text-centered">
-      <p>
-        Hast du Fragen zu den Ergebnissen? Oder weißt von Wettkämpfen die hier
-        noch fehlen?<br />
-        Melde dich bitte bei <strong>Stefan</strong>:
-        <a href="tel:004916097048114">📞 0160 970 48 114</a>&nbsp;
-        <a href="mailto:stefan@mathe.is">✉️ stefan@mathe.is</a>
-      </p>
+      <div class="container is-max-desktop">
+        <div class="columns">
+          <div class="column">
+            <div>
+              <p>Du möchtest deine eigenen Ergebnisse sehen?</p>
+              <form @submit.prevent="handleForm">
+                <div class="field">
+                  <p class="control is-expanded">
+                    <input
+                      v-model="custom_name"
+                      class="input"
+                      placeholder="Vollständiger Name"
+                    />
+                  </p>
+                  <p class="control">
+                    <button class="button is-fullwidth is-info">
+                      Meine Persönlichen Ergebnisse anzeigen
+                    </button>
+                  </p>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div class="column">
+            <p>
+              Hast du Fragen zu den Ergebnissen? Oder weißt von Wettkämpfen die
+              hier noch fehlen?<br />
+              Melde dich bitte bei <strong>Stefan</strong>:
+              <a href="tel:004916097048114">📞 0160 970 48 114</a>&nbsp;
+              <a href="mailto:stefan@mathe.is">✉️ stefan@mathe.is</a>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </footer>
 </template>
@@ -48,6 +75,14 @@
 <script setup>
 const { name: route_name, params } = useRoute();
 const { name, year } = params;
+
+const custom_name = ref("");
+const handleForm = async (event) => {
+  await navigateTo({
+    name: route_name,
+    params: { ...params, name: custom_name.value },
+  });
+};
 
 useSeoMeta({
   title: "Persönliche Bestzeiten (" + name + " @ " + year + ")",
