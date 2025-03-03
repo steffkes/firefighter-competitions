ec1c699c5a79ef0fb039fe648220014962c36ac5
steffkes <stefan@gelungen.es>
2025-03-03 17:50:18 +0100

    add redirects for missing parameters on personal pages

--- app/router.options.ts
+++ app/router.options.ts
@@ -3,8 +3,19 @@ import type { RouterConfig } from "@nuxt/schema";
 export default {
   routes: (routes) => [
     {
+      path: "/persoenlich/:name",
+      redirect: ({ params }) => ({ name: "personal-calendar", params }),
+    },
+    {
       path: "/persoenlicher-kalender/:name",
       redirect: ({ params }) => ({ name: "personal-calendar", params }),
+    },
+    {
+      path: "/persoenlich/:name/bestzeiten",
+      redirect: ({ params }) => ({
+        name: "personal-results",
+        params: { ...params, year: new Date().getFullYear() },
+      }),
     },
     {
       path: "/persoenliche-bestzeiten/:name/:year",
