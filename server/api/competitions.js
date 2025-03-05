9586d998bdbb06021ef2da88dc526108c3bb64b2
steffkes <stefan@gelungen.es>
2025-03-05 18:52:46 +0100

    cache list of competitions for one hour

--- server/api/competitions.js
+++ server/api/competitions.js
@@ -1,32 +1,34 @@
-import { defineEventHandler } from "h3"; // needed for test
 import competitionProvider from "@/competition-provider";
 import pg from "pg";
 
-export default defineEventHandler(async (event) => {
-  const data = await competitionProvider();
+export default defineCachedEventHandler(
+  async (event) => {
+    const data = await competitionProvider();
 
-  const client = new pg.Client({
-    connectionString: useRuntimeConfig(event)["DB_DSN"],
-  });
-  await client.connect();
+    const client = new pg.Client({
+      connectionString: useRuntimeConfig(event)["DB_DSN"],
+    });
+    await client.connect();
 
-  const result = await client.query(
-    `SELECT competition_id, sum(array_length(teams, 1)) as participants_count
+    const result = await client.query(
+      `SELECT competition_id, sum(array_length(teams, 1)) as participants_count
 FROM participants
 GROUP BY competition_id`,
-  );
-  await client.end();
+    );
+    await client.end();
 
-  const participantsCount = result.rows.reduce(
-    (state, entry) => ({
-      ...state,
-      [entry["competition_id"]]: parseInt(entry["participants_count"], 10),
-    }),
-    {},
-  );
+    const participantsCount = result.rows.reduce(
+      (state, entry) => ({
+        ...state,
+        [entry["competition_id"]]: parseInt(entry["participants_count"], 10),
+      }),
+      {},
+    );
 
-  return data.map((event) => {
-    event["participants"]["count"] = participantsCount[event["id"]];
-    return event;
-  });
-});
+    return data.map((event) => {
+      event["participants"]["count"] = participantsCount[event["id"]];
+      return event;
+    });
+  },
+  { maxAge: 60 * 60 /* 1 hour */ },
+);
