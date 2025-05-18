17321eed4e2800b352afcb93ba7be11984db9883
steffkes <steffkes@firefighter-competitions.actions.github.site>
2025-05-18 21:31:38 +0000

    Squashed 'data/' changes from c943ec302a..7cc1275440
    7cc1275440 Apply automatic changes
    341d22d1f7 Apply automatic changes
    a49db8ebc9 Apply automatic changes
    9f0306ef8a Apply automatic changes
    44e8720a70 Apply automatic changes
    2a0b3db1fe Apply automatic changes
    01b575f477 Apply automatic changes
    649290a614 Apply automatic changes
    5e3ec8c705 Apply automatic changes
    2d5220855a Apply automatic changes
    a93cf59c5d Apply automatic changes
    6d5d9d8cfb Apply automatic changes
    9bb48044e2 Apply automatic changes
    837a21f9e0 Apply automatic changes
    7b952007b1 Apply automatic changes
    cb7d992638 add results for schonach 2025
    9d02170f05 fix participants for schonach 2025
    e28233ed62 Apply automatic changes
    edc9a73a75 Apply automatic changes
    09d27953ce Apply automatic changes
    8ff19826ab Apply automatic changes
    80a5a3ecf1 Apply automatic changes
    48ea2174c4 Apply automatic changes
    770673681e Apply automatic changes
    61a1ef937e Apply automatic changes
    0bbda413f6 Apply automatic changes
    34b455ccfa Apply automatic changes
    ab4ed944fb Apply automatic changes
    7bd19bcd48 Apply automatic changes
    
    git-subtree-dir: data
    git-subtree-split: 7cc12754408882f80c96af0fbafbe7e231a12349

--- spider/250517_recURkcZynP4Bd4JZ_schonach.py
+++ spider/250517_recURkcZynP4Bd4JZ_schonach.py
@@ -1,6 +1,13 @@
 import scrapy
 from datetime import datetime
-from util import JsonItemExporter, JsonLinesItemExporter, ParticipantItem, ResultItem
+from util import (
+    JsonItemExporter,
+    JsonLinesItemExporter,
+    ParticipantItem,
+    ResultItem,
+    ResultRankItem,
+)
+import re
 
 
 class Spider(scrapy.Spider):
@@ -36,6 +43,27 @@ class Spider(scrapy.Spider):
         },
     }
 
+    @staticmethod
+    def fixName(name):
+        if not name:
+            return ""
+
+        return re.sub(
+            r"(([A-ZÄÜÖß]+[-\s])?([A-ZÄÜÖß]+))\s(.+)",
+            lambda match: " ".join(
+                [
+                    match.group(4),
+                    "".join(
+                        map(
+                            lambda str: str[0] + str[1:].lower(),
+                            filter(None, match.group(2, 3)),
+                        )
+                    ),
+                ]
+            ),
+            name,
+        )
+
     def start_requests(self):
         yield scrapy.FormRequest(
             method="GET",
@@ -49,46 +77,115 @@ class Spider(scrapy.Spider):
             callback=self.parse_starters,
         )
 
+        yield scrapy.FormRequest(
+            method="GET",
+            url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
+            formdata={
+                "key": self.race_key,
+                "listname": "01_Feuerwehr|Ergebnisliste_Feuerwehr_Wertungen",
+                "contest": "1",
+            },
+        )
+
     def parse_starters(self, response):
         for _team, record in response.json()["data"].items():
-            [_bib1, _number21, name1, _sex1, _category1, _year1, _competition1] = (
-                record[0]
-            )
+            [
+                _bib1,
+                _number21,
+                _ln1,
+                _fn1,
+                _gender1,
+                _competition1,
+                _yob1,
+                _category1,
+                _n31,
+                name1,
+            ] = record[0]
 
             name2 = None
             if len(record) == 2:
-                [_bib2, _number22, name2, _sex2, _category2, _year2, _competition2] = (
-                    record[1]
-                )
+                [
+                    _bib2,
+                    _number22,
+                    _ln2,
+                    _fn2,
+                    _gender2,
+                    _competition2,
+                    _yob2,
+                    _category2,
+                    _n32,
+                    name2,
+                ] = record[1]
 
             yield ParticipantItem(
                 competition_id=self.competition_id,
-                names=sorted(map(fixName, [name1, name2])),
+                names=sorted(map(self.fixName, [name1, name2])),
             )
 
+    def parse(self, response):
+        groups = []
+        entries = []
+        for group, teams in response.json()["data"].items():
+            (contest, raw_age_group) = re.match(
+                r"#\d+_((Ohne|Mit) PA) / (\w+)\s", group
+            ).group(1, 3)
+            type = "MPA" if "Mit PA" == contest else "OPA"
 
-import re
+            for team, names in teams.items():
+                [rank_age_group, raw_duration] = re.match(
+                    r"\#\d+_(\d+)\.///.*?///(.+)", team
+                ).group(1, 2)
+                [
+                    [_id11, bib, _id21, name1, gender1, _time1],
+                    [_id21, _bib2, _id22, name2, gender2, _time2],
+                ] = names
 
+                duration = "00:%s.0" % raw_duration.zfill(5)
+                gender = list(set([gender1, gender2]))
+                category = gender[0] if len(gender) == 1 else "X"
+                age_group = raw_age_group if category == "M" else None
+                groups.append((type, category, age_group))
 
-def fixName(name):
-    if not name:
-        return ""
+                result = ResultItem(
+                    date=self.race_date,
+                    competition_id=self.competition_id,
+                    type=type,
+                    duration=duration,
+                    names=sorted([self.fixName(name1), self.fixName(name2)]),
+                    category=category,
+                    bib=bib,
+                )
 
-    return re.sub(
-        r"(([A-ZÄÜÖß]+[-\s])?([A-ZÄÜÖß]+))\s(.+)",
-        lambda match: " ".join(
-            [
-                match.group(4),
-                "".join(
-                    map(
-                        lambda str: str[0] + str[1:].lower(),
-                        filter(None, match.group(2, 3)),
-                    )
-                ),
-            ]
-        ),
-        name,
-    )
+                if age_group:
+                    result["age_group"] = age_group
+
+                entries.append(result)
+
+        entries = sorted(entries, key=lambda record: record["duration"])
+
+        for group in sorted(set(groups)):
+            (type, category, age_group) = group
+
+            entries_type = list(filter(lambda entry: entry["type"] == type, entries))
+            entries_category = list(
+                filter(lambda entry: entry["category"] == category, entries_type)
+            )
+            entries_age_group = list(
+                filter(
+                    lambda entry: entry.get("age_group") == age_group, entries_category
+                )
+            )
+
+            for entry in entries_age_group:
+                entry["rank"] = ResultRankItem(
+                    total=entries_type.index(entry) + 1,
+                    category=entries_category.index(entry) + 1,
+                )
+
+                if "age_group" in entry:
+                    entry["rank"]["age_group"] = entries_age_group.index(entry) + 1
+
+                yield entry
 
 
 import pytest
@@ -105,4 +202,4 @@ import pytest
     ],
 )
 def test_fixName(input, output):
-    assert fixName(input) == output
+    assert Spider.fixName(input) == output
