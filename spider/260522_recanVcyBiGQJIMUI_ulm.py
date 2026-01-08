90b2c2b143f423846c2b443b08ffde14efa21e01
steffkes <steffkes@firefighter-competitions.actions.github.site>
2026-01-08 09:57:09 +0000

    Squashed 'data/' changes from 526ead9b01..c1d8e1394d
    c1d8e1394d Apply automatic changes
    13784aa030 Apply automatic changes
    a2493567ea Apply automatic changes
    83a35dffb3 Apply automatic changes
    c94c2f3d40 Apply automatic changes
    cb200f99fa Apply automatic changes
    90f14bcbe3 Apply automatic changes
    0c34eee5a7 fix participant names for ulm
    44b2edbc40 Apply automatic changes
    60b6884ec6 fetch participant names for ulm
    afd4458d63 Apply automatic changes
    4c766840fa Apply automatic changes
    cb9af04e2a Apply automatic changes
    e538b5693e Apply automatic changes
    
    git-subtree-dir: data
    git-subtree-split: c1d8e1394d28fdcf0b78fe0a52b1063bda0bdd48

--- spider/260522_recanVcyBiGQJIMUI_ulm.py
+++ spider/260522_recanVcyBiGQJIMUI_ulm.py
@@ -1,5 +1,6 @@
 from util import Spider, ParticipantItem, ResultItem, ResultRankItem, SlotItem
 import scrapy
+import re
 
 
 class CompetitionSpider(Spider):
@@ -8,15 +9,35 @@ class CompetitionSpider(Spider):
     race_id = "368654"
     race_key = "196b45c8a53bf1640e5b1e4250844ea4"
 
+    @staticmethod
+    def fixName(name):
+        if not name:
+            return ""
+
+        return re.sub(
+            r"(([A-ZÄÜÖß]+[-\s])?([A-ZÄÜÖß]+))\s(.+)",
+            lambda match: " ".join(
+                [
+                    match.group(4),
+                    "".join(
+                        map(
+                            lambda str: str[0] + str[1:].lower(),
+                            filter(None, match.group(2, 3)),
+                        )
+                    ),
+                ]
+            ),
+            name,
+        )
+
     def start_requests(self):
         yield scrapy.FormRequest(
             method="GET",
-            # url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
-            url="https://my.raceresult.com/%s/RRRegStart/data/config" % self.race_id,
+            url="https://my.raceresult.com/%s/RRPublish/data/list" % self.race_id,
             formdata={
                 "key": self.race_key,
                 "listname": "01_Teilnehmerlisten_RaceResult|Online-Teilnehmerliste",
-                "contest": "1",
+                "contest": "2",
             },
             callback=self.parse_starters,
         )
@@ -36,13 +57,25 @@ class CompetitionSpider(Spider):
         )
 
     def parse_starters(self, response):
-        for record in range(
-            161
-            - response.json()["RegistrationConfig"]["Registrations"][0]["Contests"][1][
-                "SlotsLeft"
-            ]
-        ):
+        for [_bib, __id1, raw_name, _gender, _origin, _team] in response.json()["data"]:
             yield ParticipantItem(
                 competition_id=self.competition_id,
-                names=[self.fixName(None)],
+                names=[self.fixName(raw_name)],
             )
+
+
+import pytest
+
+
+@pytest.mark.parametrize(
+    "input,output",
+    [
+        ("HARTMANN Jörg", "Jörg Hartmann"),
+        ("MÜLLER Naomi", "Naomi Müller"),
+        ("KNIE Nicola Simon", "Nicola Simon Knie"),
+        ("WEIßHAAR Philipp", "Philipp Weißhaar"),
+        (None, ""),
+    ],
+)
+def test_fixName(input, output):
+    assert CompetitionSpider.fixName(input) == output
